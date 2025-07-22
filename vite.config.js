91d4f8505c6e6e86f83de2a46eed8a944119e3a6
Utkarsh Bhatt <ubhatt2004@gmail.com>
2025-07-22 12:22:31 +0530

    Update dependencies and configuration for improved functionality and styling

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+// const colors = require("tailwindcss/colors");
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
+  plugins: [
+    react(),
+    tailwindcss({
+    }),
   ],
 })
